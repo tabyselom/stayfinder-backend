f1feec25541f65d8f3175e1f292589d9f08532d9
Abyselom <abyselom111@gmail.com>
2026-06-05 02:01:22 +0300

    UPDATE db

--- seeder/adminSeeder.js
+++ seeder/adminSeeder.js
@@ -6,31 +6,12 @@ const seedAdmin = async () => {
     try {
         const email = "admin@stayfinder.com";
 
-        // check if admin exists
-        const [existing] = await db.query(
-            "SELECT * FROM users WHERE email = ?",
-            [email]
-        );
-
-        if (existing.length > 0) {
-            console.log("Admin already exists");
-            process.exit();
-        }
-
+       
         const hashedPassword = await bcrypt.hash("admin123", 10);
 
-        await db.query(
-            `INSERT INTO users (full_name, phone_number, email, password, role)
-             VALUES (?, ?, ?, ?, ?)`,
-            [
-                "System Admin",
-                "0000000000",
-                email,
-                hashedPassword,
-                "admin"
-            ]
-        );
+        
 
+       console.log(hashedPassword);
         console.log("Admin created successfully");
         process.exit();
 
