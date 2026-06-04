054375093714422133a956bbb0c774307aa98e91
Abyselom <abyselom111@gmail.com>
2026-06-05 01:39:11 +0300

    UPDATE db

--- config/db.js
+++ config/db.js
@@ -1,19 +1,13 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log("DB Error:", err.message);
-        return;
-    }
-    console.log("MySQL Connected");
-});
-
-module.exports = db;
+module.exports = db.promise();
